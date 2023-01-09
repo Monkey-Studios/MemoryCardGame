77875f06c7842f2ddcaf9aede71602ea80212261
Monkey-Studios <georgeyt24@gmail.com>
2023-01-09 09:05:51 +0000

    Initial commit

---
+++ Assets/Scripts/MainCard.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCard : MonoBehaviour
+{
+    //Creates an Editable variable in the inspector
+    [SerializeField] private SceneController controller;
+    [SerializeField] private GameObject Card_Back;
+    //Checks for player clicking the card
+    public void OnMouseDown()
+    {
+        if(Card_Back.activeSelf && controller.canReveal)
+        {
+            Card_Back.SetActive(false);
+            controller.CardRevealed(this);
+        }
+    }
+    private int _id;
+    public int id
+    {
+        get { return _id; }
+    }
+
+    public void ChangeCardImage(int id, Sprite image)
+    {
+        _id = id;
+        //Gets the sprite renderer component and changes the property of the sprite.
+        GetComponent<SpriteRenderer>().sprite = image; 
+    }
+    //If the player does not correctly match cards then it will flip them back over.
+    public void Flipback()
+    {
+        Card_Back.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    //These varaibles are used to represent the cards on screen to the player
+    public const int gridRows = 2;
+    public const int gridCols = 5;
+    public const float offsetX = 35f;
+    public const float offsetY = 35f;
+
+    [SerializeField] private MainCard originalCard;
+    [SerializeField] private Sprite[] images;
+    private void Start()
+    {
+        //This is the position of the first card and all the others are then offset from this
+        Vector3 startPos = originalCard.transform.position;
+        int[] numbers = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4};
+        numbers = ShuffleArray(numbers);
+        //If we are using the main card then we keep it otherwise it instantiates a new card
+        for (int i = 0; i < gridCols; i++)
+        {
+            for(int j = 0; j < gridRows; j++ )
+            {
+                MainCard card;
+                if(i== 0 && j == 0)
+                {
+                    card = originalCard;
+                }
+                else
+                {
+                    card = Instantiate(originalCard) as MainCard;
+                }
+                //This sets the Cards position and assigns it an Image and ID
+                int index = j * gridCols + i;
+                int id = numbers[index];
+                card.ChangeCardImage(id, images[id]);
+
+                float posX = (offsetX * i) + startPos.x;
+                float posY = (offsetY * j) + startPos.y;
+                card.transform.position = new Vector3(posX, posY, startPos.z);
+            }
+        }
+    }
+    //This function shuffles the array and returns them in a new order so that the cards are random everytime
+    private int[] ShuffleArray(int[] numbers)
+    {
+        int[] newArray = numbers.Clone() as int[];
+        for(int i = 0; i <newArray.Length; i++)
+        {
+            int tmp = newArray[i];
+            int r = Random.Range(i, newArray.Length);
+            newArray[i] = newArray[r];
+            newArray[r] = tmp;
+        }
+        return newArray;
+    }
+    //This next section of the code will check for matching or none matching pairs and assign score accordingly
+    private MainCard firstRevealed;
+    private MainCard secondRevealed;
+    private int score = 0;
+    [SerializeField] private TextMesh scoreLabel;
+
+    public bool canReveal
+    {
+        get { return secondRevealed == null; }
+    }
+    //This checks for Card matches and verifys if a match has been made
+    public void CardRevealed(MainCard card)
+    {
+        if (firstRevealed == null)
+        {
+            firstRevealed = card;
+        }
+        else
+        {
+            secondRevealed = card;
+            StartCoroutine(CheckMatch());
+        }
+    }
+    private IEnumerator CheckMatch()
+    {
+        if(firstRevealed.id == secondRevealed.id)
+        {
+            score++;
+            scoreLabel.text = "Score: " + score;
+        }
+        else
+        {
+            yield return new WaitForSeconds(0.5f);
+            firstRevealed.Flipback();
+            secondRevealed.Flipback();
+        }
+        firstRevealed = null;
+        secondRevealed = null;
+    }
+}
